52a4b77652b976f200e8f704343fa16caeb59d94
Michael Hausenblas <michael.hausenblas@gmail.com>
2012-03-30 22:46:06 +0100

    minor fixes

--- graphkeeper.py
+++ graphkeeper.py
@@ -11,13 +11,13 @@ class GraphKeeper(object):
 		self.connected = False
 		self.handle = -1
 		try:
-			f = open('simple-rdf-store.log','w')
+			f = open('gk.log','w')
 			zookeeper.set_log_stream(f)
 		except IOError:
 			print "Couldn't open logfile for writing"
 
+	
 	def set_up(self):
-		self.callback_flag = False
 		self.cv = threading.Condition()
 		self.connected = False
 		def connection_watcher(handle, type, state, path):
@@ -25,37 +25,19 @@ class GraphKeeper(object):
 			self.connected = True
 			self.cv.notify()
 			self.cv.release()
-
 		self.cv.acquire()
 		self.handle = zookeeper.init(self.host, connection_watcher)
 		self.cv.wait(10.0)
 		self.cv.release()
-
 		if not self.connected:
 			raise Exception("Couldn't connect to host -", self.host)
 
-	def new_con(self):
-		cv = threading.Condition()
-		self.pending_connection = False
-		def connection_watcher(handle, type, state, path):
-			cv.acquire()
-			self.pending_connection = True
-			cv.notify()
-			cv.release()
-
-		cv.acquire()
-		handle = zookeeper.init(self.host, connection_watcher)
-		cv.wait(15.0)
-		cv.release()
-
-		if not self.pending_connection:
-			raise Exception("Couldn't connect to host -", self.host)
-		return handle
-
+	
 	def shut_down(self):
 		if self.connected:
 			zookeeper.close(self.handle)	
 
+	
 	def exists_ng(self, ng):
 		return zookeeper.exists(self.handle, ng, None)
 	
